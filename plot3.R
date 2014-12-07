807b25adda2d4c86b3f9391b39ed1b58d02f5fb1
4506445136480595 <4506445136480595@users.noreply.github.com>
2014-12-07 18:04:19 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,5 +1,5 @@
 #read in data and add column names, only read in data for 2007-02-01 and 2007-02-02 as computer has limited memory
-#create DateTime column fpr plotting
+#create DateTime column for plotting
 qdata<-read.table("house.txt", na.strings="?",header=T,skip=66636,nrows=2880,sep=";")
 names(qdata)<-c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 qdata$DateTime <- strptime(paste(qdata$Date, qdata$Time), format="%d/%m/%Y %H:%M:%S")

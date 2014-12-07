a6196d58051c1fe8570f8f483ee66d0df86d0992
4506445136480595 <4506445136480595@users.noreply.github.com>
2014-12-07 17:29:02 -0500

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,10 @@
+#read in data and add column names, only read in rows for plots as computer has limited memory
+#create DateTime column fpr plotting
+qdata<-read.table("house.txt", na.strings="?",header=T,skip=66636,nrows=2880,sep=";")
+names(qdata)<-c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+qdata$DateTime <- strptime(paste(qdata$Date, qdata$Time), format="%d/%m/%Y %H:%M:%S")
+
+#create plot1.png
+with(qdata,hist(Global_active_power, col="red", main ="Global Active Power", xlab="Global Active Power (killowats)"))
+dev.copy(png, file ="plot1.png",width=480,height=480)
+dev.off()
